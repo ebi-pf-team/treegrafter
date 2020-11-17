a0cd6440cc181c4d058d595bce15a6a6a4cf23bb
Tiago Grego <tgrego@ebi.ac.uk>
2020-11-17 09:49:43 +0000

    funcioning _runraxml and _mapto creating correct newick tree

--- treegrafter.py
+++ treegrafter.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 import re
+import os
+import json
 
 
 
-def _querymsf(query_id, matchdata, pthrAlignLength):
+def _querymsf(matchdata, pthrAlignLength):
     # matchdata contains: hmmstart, hmmend, hmmalign and matchalign, as arrays (multiple modules possible)
 
     # N-terminaly padd the sequence
@@ -18,7 +20,7 @@ def _querymsf(query_id, matchdata, pthrAlignLength):
         if i > 0:
             start = int(matchdata['hmmstart'][i])
             end = int(matchdata['hmmend'][i-1])
-            # This bridges the gap between the hits
+            # This bridges the query_idgap between the hits
             querymsf += (start - end) * '-'
 
         # extract the query string
@@ -47,10 +49,15 @@ def _querymsf(query_id, matchdata, pthrAlignLength):
     return querymsf.upper()
 
 
-def _generateFasta(pathr, query_id, querymsf):
-
+def stringify(query_id):
     # stringify query_id
+    
     query_id = re.sub('[^\w]', '_', query_id)
+
+    return query_id
+
+
+def _generateFasta(pathr, query_id, querymsf):
 
     # use static dir paths for testing. these are provided
     fasta_in_dir = '/home/tgrego/dev/treegrafter/Test/PANTHER_mini/PANTHER12_Tree_MSF/'
@@ -68,6 +75,61 @@ def _generateFasta(pathr, query_id, querymsf):
             outfile.write(infile.read())
 
     return(fasta_out_file)
+
+
+def _run_raxml(pathr, query_id, fasta_file):
+
+    print(fasta_file)
+
+    bifurnewick_in = '/home/tgrego/dev/treegrafter/Test/PANTHER_mini/PANTHER12_Tree_MSF/' + pathr + '.bifurcate.newick'
+
+    raxml_dir = '/home/tgrego/dev/treegrafter/Test/PANTHER_mini/tmp/' + \
+        pathr + '_' + query_id + '_raxml' + str(os.getpid())
+
+    os.mkdir(raxml_dir)
+
+    exit_status = os.system('raxmlHPC-PTHREADS-SSE3 -f y -p 12345 -t ' + bifurnewick_in +
+                            ' -G 0.05 -m PROTGAMMAWAG -T 4 -s ' + fasta_file + ' -n ' + pathr + ' -w ' + raxml_dir + ' >/dev/null')
+
+    print(exit_status)
+
+    mapANs = _mapto(raxml_dir, pathr, query_id)
+
+
+def _mapto(raxml_dir, pathr, query_id):
+
+    print(raxml_dir, pathr, query_id)
+
+    classification_file = raxml_dir + '/RAxML_portableTree.' + pathr + '.jplace'
+    # print(classification_file)
+
+    with open(classification_file) as classification:
+        classification_json = json.load(classification)
+
+    # print(classification_json)
+
+    locations_ref = classification_json['placements'][0]['p']
+    # print(locations_ref)
+
+    tree_string = classification_json['tree']
+    # print(tree_string)
+
+
+    matches = re.findall('AN(\d+):\d+\.\d+\{(\d+)\}', tree_string)
+    # print(matches)
+
+    AN_label = {}
+    for [an, r] in matches:
+        AN_label['AN' + an] = 'R' + r
+        AN_label['R' + r] = 'AN' + an
+
+    # print(AN_label)
+
+    newick_string = re.sub('(AN\d+)?\:\d+\.\d+{(\d+)}', 'R\g<2>', tree_string)
+    print(newick_string)
+
+
+
 
 
 
@@ -100,24 +162,25 @@ if __name__ == '__main__':
             'matchalign': [
                 'YRVFVFDLDGTLLNDNLEISEKDRRNIEKL-SRKCYVVFASGRMLVSTLNVEKKFKRTFPTIAYNGAIVYLPEEGVILNEKIPPEVAKDIIEYIKPLNVHWQAYIDDV---LSKDNEKSYARHSYRVEPNLSELVSKMGTTKLLLIDT-PERLDELKEILSERFKDVVKVFKSFPTYLEIVPKNVDKGKALRFLRERMNWKKEEIVVFGDNENDLFMFEEAGLRVAMENAIEKVKEASDIVTLTNNDSGVSYVLERI'
             ]
-        }, {
-            'hmmalign': [
-                'kpkiklialDlDGTLlnskkeiskrtlealkeakerGvkvviaTGrsraaviellkeldlgsplvtlnGalvyskqgevlfernldpevlrellelaeeegvalvaysedrssplveslhtiykepkvekvesleklleeapiskvlflstdeeklealrevleealegelsvtrsapdfleivpkgvsKgsglkrlleelgisleeviafGDgeNDlemLelaglgvamgnasekvkevadvvtasndedGvakalekyll',
-                'kpkik'
-            ],
-            'hmmend': [
-                '265',
-                '275'
-            ],
-            'hmmstart': [
-                '4',
-                '270'
-            ],
-            'matchalign': [
-                'RMPIKAVVTDLDGTLLDPQHCISNYAAEVLKKIKEKGICFIVATGRPYAEVFNRIRHCHLPDYIITSNGARIHDGAFNVVREHNLRPELVESLARVRTVKDPATNIYRGLT--PEVSAFHTDFQCTDRERFYELQ-ASELGDVHEIWFAGD-HDELVLLDNALREKYPGDLCCTFSLPHLLDCVPAGVNKGNGVREAAEMLGLALDEVACFGDGMNDESMLQVTSTSFIMANAQQRLKAVPHAQIISNADDGVAKKLEEMFF',
-                'RMPIK'
-            ]
         }
+        # , {
+        #     'hmmalign': [
+        #         'kpkiklialDlDGTLlnskkeiskrtlealkeakerGvkvviaTGrsraaviellkeldlgsplvtlnGalvyskqgevlfernldpevlrellelaeeegvalvaysedrssplveslhtiykepkvekvesleklleeapiskvlflstdeeklealrevleealegelsvtrsapdfleivpkgvsKgsglkrlleelgisleeviafGDgeNDlemLelaglgvamgnasekvkevadvvtasndedGvakalekyll',
+        #         'kpkik'
+        #     ],
+        #     'hmmend': [
+        #         '265',
+        #         '275'
+        #     ],
+        #     'hmmstart': [
+        #         '4',
+        #         '270'
+        #     ],
+        #     'matchalign': [
+        #         'RMPIKAVVTDLDGTLLDPQHCISNYAAEVLKKIKEKGICFIVATGRPYAEVFNRIRHCHLPDYIITSNGARIHDGAFNVVREHNLRPELVESLARVRTVKDPATNIYRGLT--PEVSAFHTDFQCTDRERFYELQ-ASELGDVHEIWFAGD-HDELVLLDNALREKYPGDLCCTFSLPHLLDCVPAGVNKGNGVREAAEMLGLALDEVACFGDGMNDESMLQVTSTSFIMANAQQRLKAVPHAQIISNADDGVAKKLEEMFF',
+        #         'RMPIK'
+        #     ]
+        # }
     ]
 
     test_query_id_list = iter([
@@ -136,13 +199,19 @@ if __name__ == '__main__':
         266
     ])
 
-
     for test_data in test_input_querymsf:
         test_query_id = next(test_query_id_list)
 
+        test_query_id_str = stringify(test_query_id)
 
-        query_msf = _querymsf(test_query_id, test_data, next(test_lenght))
+
+        query_msf = _querymsf(test_data, next(test_lenght))
 
         # print(query_msf)
 
-        fasta_file = _generateFasta('PTHR10000', test_query_id, query_msf)
+        fasta_file = _generateFasta('PTHR10000', test_query_id_str, query_msf)
+
+        # print(fasta_file)
+
+        result_string = _run_raxml('PTHR10000', test_query_id_str, fasta_file)
+
