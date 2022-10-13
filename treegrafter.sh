eec9ea05a95da7fef493cb262fdb2c2204ac9aed
Matthias Blum <matthiasblum@users.noreply.github.com>
2022-10-13 17:32:43 +0100

    Delete temporary file (hmmsearch results)

--- treegrafter.sh
+++ treegrafter.sh
@@ -13,4 +13,5 @@ elif [ "$1" == "search" ]; then
   treegrafterout="$4"
   hmmsearch -Z 65000000 -E 0.001 --domE 0.00000001 --incdomE 0.00000001 --notextw "$datadir"/famhmm/binHmm "$fasta" > "$hmmsearchout"
   python3 treegrafter.py run -e 0.00000001 -o "$treegrafterout" "$fasta" "$hmmsearchout" "$datadir"
+  rm "$hmmsearchout"
 fi
